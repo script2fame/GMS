9ca8a14d25353242a340c0d4fe4273451ba3991e
hungteshun <wangyiyx201495@163.com>
2017-11-11 11:33:49 +0800

    封装数据库连接,单例模式管理CourseManager对象,实现添加课程方法

--- gms/readme.txt
+++ gms/readme.txt
@@ -1,4 +1,7 @@
 v1.0
 	* 将实体模型转换成java类
-	
+v1.1
+	* 封装数据库连接Connection
+	* 建立CourseManager，实现添加课程方法	
+	* 采用单例模式取得CourseManager对象 
 	
---
+++ gms/src/com/hungteshun/gms/util/DbUtil.java
@@ -0,0 +1,61 @@
+package com.hungteshun.gms.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * 封装数据库相关操作
+ * 
+ * @author hungteshun
+ *
+ */
+public class DbUtil {
+
+	/**
+	 * 取得数据库的连接
+	 * 
+	 * @return 一个数据库连接
+	 */
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String dbUrl = "jdbc:oracle:thin:@127.0.0.1:1521:GMS";
+			String username = "hungteshun";
+			String password = "hungteshun";
+			conn = DriverManager.getConnection(dbUrl, username, password);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	public static void close(PreparedStatement pstmt) {
+		if (pstmt != null) {
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void close(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Connection conn = DbUtil.getConnection();
+		System.out.println(conn);
+	}
+}
