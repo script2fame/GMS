c91c70f903fefbd3f735a2d0b18d7f19ec01c958
hungteshun <wangyiyx201495@163.com>
2017-11-15 09:40:19 +0800

    根据学生id查询成绩信息

--- gms/src/com/hungteshun/gms/manager/GradeManagerImpl.java
+++ gms/src/com/hungteshun/gms/manager/GradeManagerImpl.java
@@ -2,10 +2,15 @@ package com.hungteshun.gms.manager;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.hungteshun.gms.model.Classes;
+import com.hungteshun.gms.model.Course;
 import com.hungteshun.gms.model.Grade;
+import com.hungteshun.gms.model.Student;
 import com.hungteshun.gms.util.DbUtil;
 
 public class GradeManagerImpl implements GradeManager {
@@ -54,8 +59,64 @@ public class GradeManagerImpl implements GradeManager {
 	}
 
 	public List<Grade> findGradeListByStudentId(int studentId) {
-		// TODO Auto-generated method stub
-		return null;
+//		//sql92
+//		select g.student_id, s.student_name, cls.classes_name, c.course_name, g.grade
+//		from t_grade g, t_student s, t_classes cls, t_course c
+//		where g.student_id=s.student_id and s.classes_id=cls.classes_id and g.course_id=c.course_id and g.student_id=10000
+
+		StringBuffer sbSql = new StringBuffer();
+		//sql99
+		sbSql.append("select g.student_id, s.student_name, cls.classes_name, c.course_name, g.grade ")
+		.append("from t_grade g join t_student s on g.student_id=s.student_id ") 
+		.append("join t_classes cls on s.classes_id=cls.classes_id ") 
+		.append("join t_course c on g.course_id=c.course_id ")
+		.append("where g.student_id=?");		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<Grade> gradeList = new ArrayList<Grade>();
+		try {
+			conn = DbUtil.getConnection();
+			pstmt = conn.prepareStatement(sbSql.toString());
+			pstmt.setInt(1, studentId);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Grade grade = new Grade();
+				
+				//学生
+				Student student = new Student();
+				student.setStudentId(rs.getInt("student_id"));
+				student.setStudentName(rs.getString("student_name"));
+				
+				//班级
+				Classes classes = new Classes();
+				classes.setClassesName(rs.getString("classes_name"));
+				
+				//建立Student和Classes的关联
+				student.setClasses(classes);
+				
+				//建立Grade和Student的关联
+				grade.setStudent(student);
+				
+				//课程
+				Course course = new Course();
+				course.setCourseName(rs.getString("course_name"));
+				
+				//建立Grade和Course的关联
+				grade.setCourse(course);
+				
+				grade.setGrade(rs.getFloat("grade"));
+				
+				gradeList.add(grade);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			DbUtil.close(rs);
+			DbUtil.close(pstmt);
+			DbUtil.close(conn); //必须关闭
+		}
+		return gradeList;
 	}
 
 	public List<Grade> findGradeListTop3() {
